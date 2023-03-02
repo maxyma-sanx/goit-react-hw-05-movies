075c618fa9214cf75846154454f27570fb6d466c
maxyma-sanx <maxymasan.dev@gmail.com>
2023-03-02 20:24:10 +0200

    update main index file

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import theme from 'utils/theme';
-import App from 'components/App';
+import { theme } from 'utils/theme';
+import { App } from 'components/App';
+import { GlobalStyles } from 'components/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="goit-react-hw-05-movies">
       <ThemeProvider theme={theme}>
         <App />
+        <GlobalStyles />
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
