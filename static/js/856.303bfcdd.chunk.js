2a70d7cd1935ac347cd62dec15d0797b4d4eb62b
maxyma-sanx <106159918+maxyma-sanx@users.noreply.github.com>
2023-03-02 18:25:39 +0000

    Deploying to gh-pages from @ maxyma-sanx/goit-react-hw-05-movies@075c618fa9214cf75846154454f27570fb6d466c 🚀

---
+++ static/js/856.303bfcdd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[856],{6856:function(t,e,a){a.d(e,{rpj:function(){return n}});var r=a(9983);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z"}}]})(t)}}}]);
+//# sourceMappingURL=856.303bfcdd.chunk.js.map
