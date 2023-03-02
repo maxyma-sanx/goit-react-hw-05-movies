ea6d8497de1e405deb4ffcc1a3788411301354d4
maxyma-sanx <maxymasan.dev@gmail.com>
2023-03-02 20:23:36 +0200

    update app, add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
-import PAGES from 'router/paths';
-import Home from 'pages/Home';
-import { GlobalStyles } from './GlobalStyles';
+import { lazy } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Header } from './Header';
 
-const App = () => {
+const Home = lazy(() => import('../pages/Home'));
+const MovieSearch = lazy(() => import('../pages/MovieSearch'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+const Cast = lazy(() => import('./Cast'));
+const Review = lazy(() => import('./Review'));
+
+export const App = () => {
   return (
     <>
       <Routes>
-        <Route path={PAGES.homepage} element={<Home />}></Route>
-        <Route path="*" element={<>Hello world!</>}></Route>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<MovieSearch />} />
+          <Route path="movies/:id" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="review" element={<Review />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-      <GlobalStyles />
     </>
   );
 };
-
-export default App;
