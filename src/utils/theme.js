58d9a0a7a3b823aa1fc47ba250bae085b7d937b5
maxyma-sanx <maxymasan.dev@gmail.com>
2023-03-02 20:19:59 +0200

    add theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,7 +1,21 @@
-const theme = {
+export const theme = {
   colors: {
     white: '#fff',
+    black: '#020509',
+    accent: '#ff0038',
+  },
+
+  size: {
+    medium: '18px',
+  },
+
+  animation: {
+    default: '500ms cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+
+  shadow: {
+    default:
+      'rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px 30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset',
+    card: 'rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;',
   },
 };
-
-export default theme;
